49e32c9cf1d37f9b3947960f7e9245dba7f27264
IlijaAngelov <ilija.angelov@yahoo.com>
2026-03-12 15:58:42 +0100

    feat: Added Controller & Cache

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,6 @@
 <?php
 
+use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    $resumeData = [];
-    try {
-        if (Storage::exists('resume.json')) {
-            $json = Storage::get('resume.json');
-            $resumeData = json_decode($json, true, 512, JSON_THROW_ON_ERROR) ?: [];
-        }
-    } catch (\Throwable $e) {
-        $resumeData = [];
-    }
-
-    return Inertia::render('index', [
-        'resumeData' => $resumeData,
-    ]);
-})->name('home');
+Route::get('/', [ResumeController::class, 'index']);
