37073aa9d4c9fcfffcc14e7bb3236f1294a7ecca
IlijaAngelov <ilija.angelov@yahoo.com>
2026-03-13 02:30:14 +0100

    chore: Remove Logging

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
@@ -11,17 +10,12 @@ class ResumeController extends Controller
 {
     public function index()
     {
-        Log::info('Request started at: '.now());
 
         $resume = Cache::remember('resumeData', now()->addDay(), function () {
-            Log::info('CACHE MISS - Reading from file at: '.now());
-
             $resumeData = Storage::get('resume.json');
 
             return json_decode($resumeData, true, 512, JSON_THROW_ON_ERROR);
         });
-
-        Log::info('Cache data retrieved successfully');
 
         return Inertia::render('index', [
             'resumeData' => $resume,
