cc415510df526e0adecfc1d1969a505a114ce5db
IlijaAngelov <ilija.angelov@yahoo.com>
2026-03-24 15:44:41 +0100

    Revert "Change home route to return welcome view"
    This reverts commit 257ce0f043b20096ea783152cea3adf269c77cc4.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,4 @@
 use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [ResumeController::class, 'index']);
