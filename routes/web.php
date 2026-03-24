e9ed17fd9b80209c68ef1e9cdcf6148be3be7044
IlijaAngelov <ilija.angelov@yahoo.com>
2026-03-24 16:24:24 +0100

    chore: Added route name

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ResumeController::class, 'index']);
+Route::get('/', [ResumeController::class, 'index'])->name('home');
